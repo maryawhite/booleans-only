89bcce30ebf5f2626764283d9f62a27ed9276ed9
Mary White <mary.a.white28@gmail.com>
2021-08-20 21:18:54 -0400

    initial file creation

---
+++ js/booleans.js
@@ -0,0 +1,8 @@
+"use strict"
+
+var string1 = "a";
+var string2 = "Mary";
+string2.includes(string1);
+console.log(string2.includes(string1));
+//the above code returns true because it is true that string2 includes string1
+
